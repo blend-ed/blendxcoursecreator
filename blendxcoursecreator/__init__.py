7016abbca3bc383540e7cb3aa9855cf529b879e1
Rabeeh T A <rabeehchemnad@gmail.com>
2025-09-26 04:48:14 +0000

    init commit

---
+++ blendxcoursecreator/__init__.py
@@ -0,0 +1,5 @@
+"""
+AI Course Creator App by Blend-ed
+"""
+
+__version__ = '0.0.1'
---
+++ blendxcoursecreator/apps.py
@@ -0,0 +1,13 @@
+"""
+blendxcoursecreator Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class BlendxcoursecreatorConfig(AppConfig):
+    """
+    Configuration for the blendxcoursecreator Django application.
+    """
+
+    name = 'blendxcoursecreator'
